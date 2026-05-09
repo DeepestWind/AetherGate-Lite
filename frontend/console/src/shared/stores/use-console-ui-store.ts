5b1f2357e431c016b3534578b34332f34b9580b9
DeepestWind <1041031300@qq.com>
2026-05-09 17:36:42 +0800

    frontend: add chat panel collapse state

--- frontend/console/src/shared/stores/use-console-ui-store.ts
+++ frontend/console/src/shared/stores/use-console-ui-store.ts
@@ -4,6 +4,17 @@ type ConsoleUiState = {
   mobileNavOpen: boolean
   setMobileNavOpen: (open: boolean) => void
   toggleMobileNav: () => void
+
+  chatLeftCollapsed: boolean
+  chatRightCollapsed: boolean
+  toggleChatLeft: () => void
+  toggleChatRight: () => void
+  setChatLeftCollapsed: (collapsed: boolean) => void
+  setChatRightCollapsed: (collapsed: boolean) => void
+
+  chatControlPanelOpen: boolean
+  toggleChatControlPanel: () => void
+  setChatControlPanelOpen: (open: boolean) => void
 }
 
 export const useConsoleUiStore = create<ConsoleUiState>((set) => ({
@@ -12,5 +23,16 @@ export const useConsoleUiStore = create<ConsoleUiState>((set) => ({
   toggleMobileNav: () =>
     set((state) => ({
       mobileNavOpen: !state.mobileNavOpen
-    }))
+    })),
+
+  chatLeftCollapsed: false,
+  chatRightCollapsed: false,
+  toggleChatLeft: () => set((state) => ({ chatLeftCollapsed: !state.chatLeftCollapsed })),
+  toggleChatRight: () => set((state) => ({ chatRightCollapsed: !state.chatRightCollapsed })),
+  setChatLeftCollapsed: (collapsed) => set({ chatLeftCollapsed: collapsed }),
+  setChatRightCollapsed: (collapsed) => set({ chatRightCollapsed: collapsed }),
+
+  chatControlPanelOpen: false,
+  toggleChatControlPanel: () => set((state) => ({ chatControlPanelOpen: !state.chatControlPanelOpen })),
+  setChatControlPanelOpen: (open) => set({ chatControlPanelOpen: open })
 }))
